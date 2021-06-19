d3357811935d2ce0ca0a86fb99b4f810a725935b
AndreaZenz <zenzero.ere@gmail.com>
2021-06-19 12:34:46 +0200

    add footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,63 @@
+<footer>
+    <div class="footer-bg">
+        <div class="container">
+            <div class="half-info">
+                <div class="info">
+                    <h2>DC COMICS</h2>
+                    <small>characters</small>
+                    <small>comics</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <h2 class="halfway">SHOP</h2>
+                    <small>shop</small>
+                    <small>comics</small>
+                </div>
+                <div class="info">
+                    <h2>DC</h2>
+                    <small>characters</small>
+                    <small>comics</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                </div>
+                <div class="info">
+                    <h2>SITES</h2>
+                    <small>characters</small>
+                    <small>comics</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                    <small>lorem</small>
+                </div>
+            </div>
+            <div class="down-info">
+                <small>All site content TM and 2020 dc entertainment, unless otherwise <a href="">noted here</a>. All rights reserved. <br><a href="">cookies setting</a></small>
+            </div>
+            <div class="half-info-r">
+                <img src="{{ asset('images/dc-logo-bg.png') }}" alt="">
+            </div>
 
+        </div>
+    </div>
+    <div class="footer-nav-bg">
+        <div class="container footer-bar">
+            <div class="sign-up-button">
+                <p> <a href="">SIGN-UP NOW!</a> </p>
+            </div>
+            <div class="follow-us">
+                <a href="#">FOLLOW US</a>
+                <a href=""><img src="{{asset("images/footer-facebook.png")}}" alt="fb"></a>
+                <a href=""><img src="{{asset("images/footer-twitter.png")}}" alt="tw"></a>
+                <a href=""><img src="{{asset("images/footer-youtube.png")}}" alt="yt"></a>
+                <a href=""><img src="{{asset("images/footer-pinterest.png")}}" alt="pint"></a>
+                <a href=""><img src="{{asset("images/footer-periscope.png")}}" alt="periscope"></a>
+            </div>
+        </div>
+    </div>
+</footer>
