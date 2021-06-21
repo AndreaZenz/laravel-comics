09ee2748b222d5ee00bbcde1898bebab1f3e739c
AndreaZenz <zenzero.ere@gmail.com>
2021-06-21 16:53:33 +0200

    Complete single-comic page

--- resources/views/partials/body-comics.blade.php
+++ resources/views/partials/body-comics.blade.php
@@ -31,22 +31,75 @@
         </div>
     </div>
 </div>
-<div class="container splitter">
-    <div class="half">
-        <h2>Talent</h2>
-        <div class="content">
-            <p>Art by:</p>
-            @foreach($comic as $artist)
-                <a href=""></a>
-            @endforeach
-            <a href="#"></a>
+<div class="container-splitter">
+    <div class="splitter">
+        <div class="half">
+            <h2>Talent</h2>
+            <div class="content border-top">
+                <div class="title">
+                    <p>Art by:</p>
+                </div>
+                <div class="title-text">
+                    @for($i = 0; $i < count($comic['artists']); $i++) <a href="#">{{$comic['artists'][$i]}}</a><span>,</span>
+                        @endfor
+                </div>
+            </div>
+            <div class="content">
+                <div class="title">
+                    <p>Written by:</p>
+                </div>
+                <div class="title-text">
+                    @for($i = 0; $i < count($comic['writers']); $i++) <a href="#">{{$comic['writers'][$i]}}</a><span>,</span>
+                        @endfor
+                </div>
+            </div>
         </div>
-        <div class="content">
-            <p>Written by:</p>
-            <a href="#">{{ $comic['writers'] }}</a>
+        <div class="half">
+            <h2>Specs</h2>
+            <div class="content border-top">
+                <div class="title">
+                    Series:
+                </div>
+                <div class="title-text">
+                    <a href="#">{{$comic['series']}}</a>
+                </div>
+            </div>
+            <div class="content">
+                <div class="title">
+                    U.S Price:
+                </div>
+                <div class="title-text">
+                    <a href="#">{{$comic['price']}}</a>
+                </div>
+            </div>
+            <div class="content">
+                <div class="title">
+                    On Sale Date:
+                </div>
+                <div class="title-text">
+                    <a href="#">{{$comic['sale_date']}}</a>
+                </div>
+            </div>
         </div>
     </div>
-    <div class="half">
-        <h2>Specs</h2>
+</div>
+<div class="comic-bar border-top">
+    <div class="container-less h100">
+        <div class="details">
+            <p>DIGITAL COMICS</p>
+            <img src="{{ asset('images/1.png') }}" alt="">
+        </div>
+        <div class="details">
+            <p>SHOP DC</p>
+            <img src="{{ asset('images/2.png') }}" alt="">
+        </div>
+        <div class="details">
+            <p>COMIC SHOP LOCATOR</p>
+            <img src="{{ asset('images/3.png') }}" alt="">
+        </div>
+        <div class="details">
+            <p>SUBSCRIPTION</p>
+            <img src="{{ asset('images/4.png') }}" alt="">
+        </div>
     </div>
 </div>
